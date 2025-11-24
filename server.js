93cefc5339e5ef3ab0e7ef208569ba0f59cd6590
tsion beyene <tsibeyeu@gmail.com>
2025-11-25 00:01:23 +0300

    initial commit backend

---
+++ server.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import cors from 'cors'
+import  'dotenv/config'
+import connectDB from './config/mongodb.js'
+
+// App Config
+const app = express()
+const port = process.env.PORT || 4000
+
+connectDB()
+
+// Middlewares
+app.use(express.json())
+app.use(cors())
+
+// API Endpoints
+app.get('/', (req, res) => {
+  res.send('Hello from the backend!')
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`)
+})
