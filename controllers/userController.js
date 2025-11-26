cfe348e1502ef99fcbf46e9e7048e66b6a3e89d6
tsion beyene <tsibeyeu@gmail.com>
2025-11-26 22:33:48 +0300

    add product and authetication done

---
+++ controllers/userController.js
@@ -0,0 +1,68 @@
+import UserModel from "../models/userModel.js";
+import validator from "validator";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const createToken=(id)=>{
+    return jwt.sign({id},process.env.JWT_SECRET,{
+        expiresIn:'7d',
+    });
+
+}
+
+// user login controller
+const loginUser=async (req,res)=>{
+
+}
+
+// user registration controller
+const registerUser=async (req,res)=>{
+   try {
+    const {name,email,password}=req.body;
+    // checking if user already exists
+
+    const exists =await UserModel.findOne({email});
+    if(exists){
+        return res.status(400).json({success:false,message:"User already exists"})
+    }
+
+    // validating email and strong password
+    if(!validator.isEmail(email)){
+        return res.status(400).json({message:"Invalid email"})
+    }
+    if(password.length<8){
+        return res.status(400).json({message:"Password must be at least 8 characters long"})
+    }
+
+    // hashing password
+    const salt= await bcrypt.genSalt(10);
+    const hashedPassword= await bcrypt.hash(password,salt);
+    // const hashedPassword=await bcrypt.hash(password,10);
+    // creating new user
+    const newUser=new UserModel({
+        name,
+        email,
+        password:hashedPassword,
+    });
+
+    const user =await newUser.save() ;
+
+    const token =createToken(user._id);
+
+    res.json({ success:true,token})
+    
+   } catch (error) {
+    console.error("Error in registerUser:", error);
+    res.json({success:false,message:error.message})
+    
+   }
+}
+
+
+//Admin login controller
+const adminLogin=async (req,res)=>{
+
+}
+
+
+export {loginUser, registerUser,adminLogin};
--- server.js
+++ server.js
@@ -2,18 +2,24 @@ import express from 'express'
 import cors from 'cors'
 import  'dotenv/config'
 import connectDB from './config/mongodb.js'
+import connectCloudinary from './config/cloudinary.js'
+import userRouter from './routes/userRoute.js'
+import productRouter from './routes/productRoute.js'
 
 // App Config
 const app = express()
 const port = process.env.PORT || 4000
 
 connectDB()
+connectCloudinary()
 
 // Middlewares
 app.use(express.json())
 app.use(cors())
 
 // API Endpoints
+app.use('/api/user',userRouter)
+app.use('/api/product',productRouter)
 app.get('/', (req, res) => {
   res.send('Hello from the backend!')
 })
