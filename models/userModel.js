7559c8e3b64b75ec61995c3b86ba6f3d4004994a
tsion beyene <tsibeyeu@gmail.com>
2026-04-05 12:19:55 +0300

    change made on model

--- models/userModel.js
+++ models/userModel.js
@@ -1,27 +1,27 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     email: {
-        type: String,
-        required: true,
-        unique: true,
+      type: String,
+      required: true,
+      unique: true,
     },
     password: {
-        type: String,   
-        required: true,
+      type: String,
+      required: true,
     },
-    cartData:{
-        type:Object,
-        default:{}
-    }
+    cartData: {
+      type: Object,
+      default: {},
+    },
+  },
+  { minimize: false },
+);
 
-   
-    
-}, {minimize: false});
-
-const UserModel = mongoose.models.user || mongoose.model('User', userSchema);
+const UserModel = mongoose.models.user || mongoose.model("User", userSchema);
 export default UserModel;
